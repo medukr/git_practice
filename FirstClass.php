ce6b096acbe7063ecd4003aa9f2080b1a2b8ee1b
medukr <andrii.demydyuk@gmail.com>
2019-10-11 11:24:18 +0300

    refactored FirstClass

--- FirstClass.php
+++ FirstClass.php
@@ -5,12 +5,14 @@ class FirstClass
 
     private $some_data;
 
-    public function getSomeData(){
-        return $this->some_data;
+
+    public function __construct($data)
+    {
+        $this->some_data = $data;
     }
 
-    public function setSomeData($data){
-        $this->some_data = $data;
+    public function getSomeData(){
+        return $this->some_data;
     }
 
 }
